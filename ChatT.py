b9b5a76fe5b5989ed5463dbe31bc51f5e314c88e
Tom <tom.heiss@outlook.at>
2025-05-24 09:39:11 +0200

    Initialer Commit meines Python-Programms

---
+++ ChatT.py
@@ -0,0 +1,18 @@
+# ChatT.py
+
+print('ChatT: Hallo, ich bin ChatT. Was ist los?')
+
+eingabe = input('Du: ')
+if 'hass' in eingabe:
+    print('ChatT: Hass bringt dich nicht weiter.')
+
+else:
+    if 'liebe' in eingabe:
+        print('ChatT: Liebe ist Spitze!')
+
+    else:
+        if 'schlaf' in eingabe:
+            print('ChatT: Schlaf ist wichtig')
+        else:
+            print('ChatT: Kannst du mir Details geben?')
+    
